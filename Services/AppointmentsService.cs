cd3a05085760801be06cc54ccca9f8874e61ded9
s33229 <s33229@pjwstk.edu.pl>
2026-04-25 13:33:01 +0200

    Add empty service

---
+++ Services/AppointmentsService.cs
@@ -0,0 +1,31 @@
+using APBD_6.DTOs;
+
+namespace APBD_6.Services;
+
+public class AppointmentsService : IAppointmentsService
+{
+  public int AddAppointment()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void DeleteAppointment()
+  {
+    throw new NotImplementedException();
+  }
+
+  public IEnumerable<AppointmentListDto> GetAllAppointments()
+  {
+    throw new NotImplementedException();
+  }
+
+  public AppointmentDetailsDto GetAppointmentById(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void UpdateAppointment(int id, CreateAppointmentRequestDto appointment)
+  {
+    throw new NotImplementedException();
+  }
+}
